c0ba0710280c62016571fc06faeffdc3985dc31c
Paul M. Rodriguez <pmr@ruricolist.com>
2013-11-14 14:07:13 -0600

    More efficient templates.

--- templates.lisp
+++ templates.lisp
@@ -2,11 +2,11 @@
 
 (export '(deftemplate))
 
-(defmacro deftemplate (name args &body body)
+(defmacro deftemplate (name args &body body &environment env)
   (if (null args)
       `(defun ,name () (with-html ,@body))
       (multiple-value-bind (new-arglist gensym-pairs)
-          (escape-arglist args)
+          (escape-arglist args env)
         (let ((vars (mapcar #'car gensym-pairs)))
           (with-gensyms (whole)
             `(eval-and-compile
@@ -14,73 +14,105 @@
                      (compile nil
                               (lambda ,new-arglist
                                 (symbol-macrolet ,(loop for (v . g) in gensym-pairs
-                                                        collect `(,v (fcall ,g)))
+                                                        collect `(,v (eval* ,g)))
                                   (with-html ,@body)))))
                (defmacro ,name (&whole ,whole ,@args)
                  (declare (ignore ,@vars))
                  `(call-template ',',name ,@(cdr ,whole)))))))))
 
-(defmacro call-template (name &rest args &environment env)
-  `(funcall
-    (get ,name 'template)
-    ,@(loop for arg in args
-            if (constantp arg env)
-              collect arg
-            else collect `(delay (with-html ,arg)))))
-
-(defmacro delay (exp)
-  (let ((value (gensym)))
-    `(let (,value)
-       (lambda ()
-         (or ,value (setf ,value ,exp))))))
-
-(defun force (thunk)
-  (if (functionp thunk)
-      (funcall thunk)
-      thunk))
+(defmacro call-template (name &rest args)
+  (flet ((string-gensym (x)
+           (gensym (string x)))
+         (could-be-keyword? (x)
+           (constantp x)))
+    (let* ((params (remove-if #'could-be-keyword? args))
+           (cells
+             (loop for param in params
+                   unless (could-be-keyword? param)
+                     collect (string-gensym 'cell)))
+           (guards
+             (loop for nil in params
+                   collect (string-gensym 'guard)))
+           (thunks
+             (loop for nil in params
+                   collect (string-gensym 'thunk)))
+           (fns
+             (loop for thunk in thunks
+                   collect `(function ,thunk))))
+      `(let (,@cells ,@guards)
+         (flet ,(loop for param in params
+                      for cell in cells
+                      for guard in guards
+                      for thunk in thunks
+                      collect `(,thunk
+                                ()
+                                (if ,guard
+                                    ,cell
+                                    (setf ,guard t
+                                          ,cell (with-html ,param)))))
+           (declare (dynamic-extent ,@fns))
+           (funcall
+            (get ,name 'template)
+            ,@(loop for arg in args
+                    if (could-be-keyword? arg)
+                      collect arg
+                    else collect (pop fns))))))))
 
 (defvar *quote* nil)
 
-(defun fcall (x)
+(defun eval* (x)
   (if *quote*
       x
       (typecase x
-        (list (mapc #'force x))
-        (function (force x))
+        (list (mapc #'funcall x))
+        (function (funcall x))
         (t x))))
 
 (defmacro do-elements ((var arg) &body body)
   (with-gensyms (temp)
-    `(dolist (,temp (let ((*quote* t)) ,arg))
-       (symbol-macrolet ((,var (fcall ,temp)))
+    `(dolist (,temp (let ((*quote* t))
+                      ,arg))
+       (symbol-macrolet ((,var (eval* ,temp)))
          ,@body))))
 
-(defun escape-arglist (arglist)
+(defun escape-arglist (arglist env)
   ;; Returns two values: the escaped arglist and an alist of variable
   ;; names and gensyms.
   (multiple-value-bind (required optional rest
                         keywords allow-other-keys aux)
       (alexandria:parse-ordinary-lambda-list (substitute '&rest '&body arglist))
     (let ((gensym-pairs '()))
-      (labels ((mkgensym (arg)
-                 (or (cdr (rassoc arg gensym-pairs :test #'string=))
-                     (let ((g (gensym (string arg))))
-                       (prog1 g
-                         (setf gensym-pairs (cons (cons arg g) gensym-pairs)))))))
+      ;; TODO Rewrite the initforms?
+      (flet ((string-gensym (arg)
+               (gensym (string arg)))
+             (unsplice (x)
+               (if x (list x) nil))
+             (check-initform (initform)
+               (unless (constantp initform env)
+                 (error "Initforms must be constant."))))
         (values
          (append (loop for arg in required
-                       collect (mkgensym arg))
+                       for g = (string-gensym arg)
+                       do (push (cons arg g) gensym-pairs)
+                       collect g)
                  (when optional
                    (cons '&optional
                          (loop for (name init suppliedp) in optional
-                               collect `(,(mkgensym name) ,init
-                                         ,@(when suppliedp (list suppliedp))))))
+                               for g = (string-gensym name)
+                               do (check-initform init)
+                                  (push (cons name g) gensym-pairs)
+                               collect `(,g ,init ,@(unsplice suppliedp)))))
                  (when rest
-                   (list '&rest (mkgensym rest)))
+                   (let ((g (string-gensym rest)))
+                     (push (cons rest g) gensym-pairs)
+                     (list '&rest g)))
                  (when keywords
                    (cons '&key
                          (loop for ((kw-name name) init supplied) in keywords
-                               collect `((,kw-name ,(mkgensym name)) ,init))))
+                               for g = (string-gensym name)
+                               do (check-initform init)
+                                  (push (cons name g) gensym-pairs)
+                               collect `((,kw-name ,g) ,init))))
                  (when allow-other-keys
                    (list '&allow-other-keys))
                  (when aux (cons '&aux (sublis gensym-pairs aux))))
