9f9df420cfe5897b241050a8fe44af4d736e8ea5
Paul M. Rodriguez <pmr@ruricolist.com>
2017-03-17 18:27:29 -0500

    Disable elastic newlines.

--- stream.lisp
+++ stream.lisp
@@ -1,41 +1,54 @@
 (in-package :spinneret)
 
-(progn
-  (deftype elastic-newline ()
-    '(eql #1=:elastic-newline))
-  (serapeum:defconst elastic-newline #1#))
-
-(defclass html-stream (fundamental-character-input-stream)
-  ((col :type (integer 0 *) :initform 0)
+(defclass html-stream (fundamental-character-output-stream)
+  ((col :type (integer 0 *) :initform 0
+        :reader html-stream-column)
    (line :type (integer 0 *) :initform 0)
-   (last-char :type (or character elastic-newline)
+   (last-char :type character
               ;; The last char defaults to newline to get reasonable
               ;; behavior from fresh-line.
-              :initform #\Newline
-              :accessor .last-char)
+              :initform #\Newline)
    (base-stream :type stream
                 :initarg :base-stream)))
 
 (defun make-html-stream (base-stream)
   (make-instance 'html-stream
-                 :base-stream base-stream))
+                 :base-stream (assure stream base-stream)))
+
+(defgeneric html-stream? (x)
+  (:method ((x t)) nil))
+
+(defgeneric ensure-html-stream (x)
+  (:method ((x stream))
+    (if *print-pretty*
+        (make-html-stream x)
+        (assure stream x))))
 
 (serapeum:defmethods html-stream (s col line last-char base-stream)
+  (:method ensure-html-stream (s)
+    s)
+
+  (:method html-stream? (s)
+    t)
+
   (:method stream-line-column (s)
     col)
 
   (:method stream-start-line-p (s)
     (= col 0))
 
+  (:method stream-write-char (s (char (eql #\Newline)))
+    ;; (nix elastic-newline)
+    (incf line)
+    (setf col 0)
+    (setf last-char #\Newline)
+    (terpri base-stream)
+    #\Newline)
+
   (:method stream-write-char (s char)
-    (when (eql last-char elastic-newline)
-      (unless (eql char #\Newline)
-        (write-char #\Newline base-stream)
-        (incf line)
-        (setf col 0)))
-    (when (eql char #\Newline)
-      (incf line)
-      (setf col 0))
+    ;; (when (nix elastic-newline)
+    ;;   (write-char #\Newline s))
+    (incf col 1)
     (write-char char base-stream)
     (setf last-char char)
     char)
@@ -45,22 +58,24 @@
           (start (or start 0)))
       (declare (type array-index start end))
       (nlet rec ((start start))
-        (let ((nl (or (position #\Newline string :start start :end end)
-                      end)))
-          (write-string string base-stream :start start :end nl)
-          (when (> (- nl start) 0)
-            (when (eql last-char elastic-newline)
-              (incf line)
-              (setf col 0)
-              (terpri base-stream))
-            (let ((end-char (aref string (1- nl))))
-              (setf last-char end-char)))
-          (incf col (- nl start))
-          (unless (= nl end)
-            (incf line)
-            (setf col 0)
-            (terpri base-stream)
-            (rec (1+ nl))))))
+        ;; (when (nix elastic-newline)
+        ;;   (unless (serapeum:string^= #\Newline string)
+        ;;     (incf line)
+        ;;     (setf col 0)
+        ;;     (terpri base-stream)))
+        (let ((newline-count (count #\Newline string :start start :end end)))
+          (if (= newline-count 0)
+              (progn
+                (write-string string base-stream :start start :end end)
+                (incf col (- end start)))
+              (progn
+                (incf line newline-count)
+                (setf col (- end (position #\Newline string
+                                           :from-end t
+                                           :end end
+                                           :start start))))))
+        (when (> (- end start) 1)
+          (setf last-char (aref string (1- end))))))
     string)
 
   (:method stream-terpri (s)
@@ -85,5 +100,6 @@
         (write-char #\Space s)))
     t)
 
-  (:method elastic-newline (s)
-    (setf last-char elastic-newline)))
+  ;; (:method elastic-newline (s)
+  ;;   (setf elastic-newline t))
+  )
