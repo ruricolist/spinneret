f4508cc1dca99d2935d6ced6a4936cb3bf4341a0
Paul M. Rodriguez <pmr@ruricolist.com>
2017-03-27 13:17:43 -0500

    Update lorem ipsum test.

--- tests.lisp
+++ tests.lisp
@@ -227,19 +227,18 @@
 <html lang=en>
  <body>
   <div>
-   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+    culpa qui officia deserunt mollit anim id est laborum.<span></span><a href=\"\"
+    data-instant=true>Hello</a> Lorem ipsum dolor sit amet, consectetur adipiscing
+    elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
     non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    <span></span><a href=\"\" data-instant=true>Hello</a> Lorem ipsum dolor sit
-    amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-    in culpa qui officia deserunt mollit anim id est laborum.
   </div>
  </body>
 </html>")
@@ -375,13 +374,10 @@
         data-instant=true></div>
   </div>
  </div>
-</div>
-")))
-
-    (with-html-string
-      (:div
-        (:div
-          (:div
-            (:div.list-group.toc-entries
-             :data-instant t))))))
-  )
+</div>")
+         (with-html-string
+           (:div
+             (:div
+               (:div
+                 (:div.list-group.toc-entries
+                  :data-instant t)))))))))
