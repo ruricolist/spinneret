8a0c81f3d8440aec095ffc59a6a5e56579749847
Paul M. Rodriguez <pmr@ruricolist.com>
2022-07-23 10:18:17 -0500

    Add math and svg as HTML5 elements

--- tags.lisp
+++ tags.lisp
@@ -88,9 +88,9 @@
     :datalist :dd :del :details :dfn :div :dl :dt :em :embed :fieldset
     :figcaption :figure :footer :form :head :h1 :h2 :h3 :h4 :h5 :h6 :header
     :hgroup :hr :html :i :iframe :img :input :ins :kbd :keygen :label :legend :li
-    :link :main :map :mark :menu :meta :meter :nav :noscript :object :ol
+    :link :main :map :mark :math :menu :meta :meter :nav :noscript :object :ol
     :optgroup :option :output :p :param :picture :pre :progress :q :rp :rt :ruby :s :samp
-    :script :section :select :small :source :span :strong :style :sub :summary
+    :script :section :select :small :source :span :strong :style :sub :svg :summary
     :sup :table :tbody :td :template :textarea :tfoot :th :thead :time :title :tr
     :track :u :ul :var :video :wbr))
 
